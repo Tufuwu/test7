0b2d97a3717a3b424b0658d56038294de29f57f3
wu <2450402591@qq.com>
2025-03-12 23:40:31 +0800

    decalage2/olefile/gpt-4o

--- setup.py
+++ setup.py
@@ -1,74 +1,88 @@
-#! /usr/bin/env python
-# coding=utf-8
+"""
+Installation script for olefile using setuptools
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+To install this package, run:
+    python setup.py install
+
+"""
 
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+# --- IMPORTS -----------------------------------------------------------------
 
-with open('CHANGELOG.rst') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
+from setuptools import setup
 
-requirements = [
+from olefile import __version__, __author__
+
+
+# --- METADATA ----------------------------------------------------------------
+
+name         = "olefile"
+version      = __version__
+desc         = "Python package to parse, read and write Microsoft OLE2 files (Structured Storage or Compound Document, Microsoft Office)"
+author       = __author__
+author_email = "nospam@decalage.info"
+url          = "https://www.decalage.info/python/olefileio"
+license      = "BSD"
+download_url = "https://github.com/decalage2/olefile/tarball/master"
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Information Technology",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules"
 ]
 
-test_requirements = [
-    # TODO: put package test requirements here
+# --- PACKAGES ----------------------------------------------------------------
+
+packages = [
+    "olefile",
 ]
 
-setup(
-    name='python-nvd3',
-    version='0.16.0',
-    description="Python NVD3 - Chart Library for d3.js",
-    long_description=readme + '\n\n' + history,
-    keywords='plot, graph, nvd3, d3',
-    author='Belaid Arezqui',
-    author_email='areski@gmail.com',
-    url='http://github.com/areski/python-nvd3',
-    license="MIT",
-    py_modules=['nvd3'],
-    namespace_packages=[],
-    test_suite='tests',
-    packages=[
-        'nvd3',
-    ],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'python-slugify>=1.2.5',
-        'Jinja2>=2.8'
-        # -*- Extra requirements: -*-
-    ],
-    entry_points={
-        'console_scripts': [
-            'nvd3 = nvd3.NVD3Chart:_main',
-        ],
-    },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Topic :: Multimedia :: Graphics :: Presentation',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-)
+
+# === MAIN ====================================================================
+
+def main():
+    setup(
+        name=name,
+        version=version,
+        description=desc,
+        long_description=long_desc,
+        long_description_content_type='text/markdown',
+        classifiers=classifiers,
+        author=author,
+        author_email=author_email,
+        url=url,
+        license=license,
+#        package_dir=package_dir,
+        packages=packages,
+        download_url=download_url,
+#        data_files=data_files,
+#        scripts=scripts,
+        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    )
+
+
+if __name__ == "__main__":
+    main()
