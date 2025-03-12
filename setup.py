b3025b9fe7324edf6244c2ee9837695a20598099
wu <2450402591@qq.com>
2025-03-12 23:52:18 +0800

    pogzyb/whodap/gpt-4o

--- setup.py
+++ setup.py
@@ -1,22 +1,50 @@
-import sys
-
-from os import path
 from setuptools import setup
+import pathlib
 
-base_dir = path.dirname(__file__)
-src_dir = path.join(base_dir, 'pyvips')
 
-# When executing the setup.py, we need to be able to import ourselves, this
-# means that we need to add the pyvips/ directory to the sys.path.
-sys.path.insert(0, src_dir)
+base_dir = pathlib.Path(__file__).parent.resolve()
+long_description = (base_dir / 'README.md').read_text(encoding='utf-8')
 
-# Try to install in API mode first, then if that fails, fall back to ABI
 
-# API mode requires a working C compiler plus all the libvips headers whereas
-# ABI only needs the libvips shared library to be on the system
+def get_version(location: str) -> str:
+    with open((base_dir / location).absolute().resolve()) as file:
+        for line in file.readlines():
+            if line.startswith('__version__'):
+                return line.split(' = ')[-1].strip().replace("\"", "")
+        else:
+            raise RuntimeError('Unable to find version string.')
 
-try:
-    setup(cffi_modules=['pyvips/pyvips_build.py:ffibuilder'])
-except Exception as e:
-    print(f'Falling back to ABI mode. Details: {e}')
-    setup()
+
+setup(
+    name='whodap',
+    version=get_version('whodap/__init__.py'),
+    description='Simple RDAP Utility for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pogzyb/whodap',
+    author='Joseph Obarzanek',
+    author_email='pogzyb@umich.edu',
+    python_requires='>=3.6, <4',
+    keywords='security, whois, rdap, research',
+    install_requires=[
+        'httpx>=0.20.0',
+        'async_generator>=1.10; python_version < "3.7.0"'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Security',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=['whodap'],
+    package_dir={'whodap': 'whodap'},
+)
