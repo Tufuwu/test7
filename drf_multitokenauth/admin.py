67770dd8a1bc7c1289187680491bc63212b7b0bc
wu <2450402591@qq.com>
2025-03-13 00:29:24 +0800

    provinzkraut/cologne_phonetics/gpt-4o

--- drf_multitokenauth/admin.py
+++
@@ -1,8 +0,0 @@
-""" contains basic admin views for MultiToken """
-from django.contrib import admin
-from drf_multitokenauth.models import MultiToken
-
-
-@admin.register(MultiToken)
-class MultiTokenAdmin(admin.ModelAdmin):
-    list_display = ('user', 'name', 'key', 'user_agent')
--- tests/urls.py
+++
@@ -1,7 +0,0 @@
-""" Tests App URL Config """
-from django.conf.urls import include
-from django.urls import re_path
-
-urlpatterns = [
-    re_path(r'^api/auth/', include('drf_multitokenauth.urls', namespace='multi_token_auth')),
-]
