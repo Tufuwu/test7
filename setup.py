9768e9c1be612b77906e21a3b223cea85d1165ca
wu <2450402591@qq.com>
2025-03-13 00:20:58 +0800

    pycqa/pyflakes/gpt-4o

--- setup.py
+++ setup.py
@@ -1,61 +1,59 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup
-import os
-import re
+#!/usr/bin/env python
+# Copyright 2005-2011 Divmod, Inc.
+# Copyright 2013 Florent Xicluna.  See LICENSE file for details
+import os.path
 
-# Lovingly adapted from https://github.com/kennethreitz/requests/blob/39d693548892057adad703fda630f925e61ee557/setup.py#L50-L55
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pusher/version.py'), 'r') as fd:
-    VERSION = re.search(r'^VERSION = [\']([^\']*)[\']',
-                        fd.read(), re.MULTILINE).group(1)
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    extra = {'scripts': ["bin/pyflakes"]}
+else:
+    extra = {
+        'test_suite': 'pyflakes.test',
+        'entry_points': {
+            'console_scripts': ['pyflakes = pyflakes.api:main'],
+        },
+    }
 
-if not VERSION:
-    raise RuntimeError('Ensure `VERSION` is correctly set in ./pusher/version.py')
+
+def get_version(fname=os.path.join('pyflakes', '__init__.py')):
+    with open(fname) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return eval(line.split('=')[-1])
+
+
+def get_long_description():
+    descr = []
+    for fname in ('README.rst',):
+        with open(fname) as f:
+            descr.append(f.read())
+    return '\n\n'.join(descr)
+
 
 setup(
-    name='pusher',
-    version=VERSION,
-    description='A Python library to interract with the Pusher Channels API',
-    long_description='A Python library to interract with the Pusher Channels API',
-    long_description_type='text/x-rst',
-    url='https://github.com/pusher/pusher-http-python',
-    author='Pusher',
-    author_email='support@pusher.com',
+    name="pyflakes",
+    license="MIT",
+    version=get_version(),
+    description="passive checker of Python programs",
+    long_description=get_long_description(),
+    author="A lot of people",
+    author_email="code-quality@python.org",
+    url="https://github.com/PyCQA/pyflakes",
+    packages=["pyflakes", "pyflakes.scripts", "pyflakes.test"],
+    python_requires='>=3.8',
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Internet :: WWW/HTTP',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 6 - Mature",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
     ],
-    keywords='pusher rest realtime websockets service',
-    license='MIT',
-
-    packages=[
-        'pusher'
-    ],
-
-    install_requires=[
-        'six',
-        'requests>=2.3.0',
-        'urllib3',
-        'pyopenssl',
-        'ndg-httpsclient',
-        'pyasn1',
-        'pynacl'
-    ],
-
-    tests_require=['nose', 'mock', 'HTTPretty'],
-
-    extras_require={
-        'aiohttp': ['aiohttp>=0.20.0'],
-        'tornado': ['tornado>=5.0.0']
-    },
-
-    package_data={
-        'pusher': ['cacert.pem']
-    },
-
-    test_suite='pusher_tests',
-)
+    **extra)
