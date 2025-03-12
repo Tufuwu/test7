69e1a927af3e8053c6f5ab899a3c13f72f3bd91a
wu <2450402591@qq.com>
2025-03-12 23:52:53 +0800

    bodik/soft-webauthn/gpt-4o

--- setup.py
+++ setup.py
@@ -1,50 +1,24 @@
-from setuptools import setup
-import pathlib
+import setuptools
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
-base_dir = pathlib.Path(__file__).parent.resolve()
-long_description = (base_dir / 'README.md').read_text(encoding='utf-8')
-
-
-def get_version(location: str) -> str:
-    with open((base_dir / location).absolute().resolve()) as file:
-        for line in file.readlines():
-            if line.startswith('__version__'):
-                return line.split(' = ')[-1].strip().replace("\"", "")
-        else:
-            raise RuntimeError('Unable to find version string.')
-
-
-setup(
-    name='whodap',
-    version=get_version('whodap/__init__.py'),
-    description='Simple RDAP Utility for Python',
+setuptools.setup(
+    name='soft-webauthn',
+    version='0.1.4',
+    author='Radoslav Bodó',
+    author_email='bodik@cesnet.cz',
+    description='Python webauthn software authenticator',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/pogzyb/whodap',
-    author='Joseph Obarzanek',
-    author_email='pogzyb@umich.edu',
-    python_requires='>=3.6, <4',
-    keywords='security, whois, rdap, research',
+    url='https://github.com/bodik/soft-webauthn',
+    py_modules=['soft_webauthn'],
     install_requires=[
-        'httpx>=0.20.0',
-        'async_generator>=1.10; python_version < "3.7.0"'
+        'fido2>=1.0,<2.0',
+        'cryptography'
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'Topic :: Security',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
     ],
-    packages=['whodap'],
-    package_dir={'whodap': 'whodap'},
 )
