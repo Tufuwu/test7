9881682b9b9624f2b436d396be5250732935121e
wu <2450402591@qq.com>
2025-03-13 00:19:49 +0800

    fatal1ty/aioapns/gpt-4o

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
-from setuptools import setup
-import io
-import os
+#!/usr/bin/env python
 
-from version import get_version
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-short_description = 'Various BM25 algorithms for document ranking'
-
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = short_description
+from setuptools import find_packages, setup
 
 setup(
-    name='rank_bm25',
-    version=get_version(),
-    description=short_description,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='D. Brown',
-    author_email='dorianstuartbrown@gmail.com',
-    url="https://github.com/dorianbrown/rank_bm25",
-    license='Apache2.0',
-    py_modules=['rank_bm25'],
-    install_requires=['numpy'],
-    extras_require={
-        'dev': [
-            'pytest'
-        ]
-    }
+    name="aioapns",
+    version="3.3.1",
+    description="An efficient APNs Client Library for Python/asyncio",
+    long_description=open("README.rst").read(),
+    platforms="all",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    license="Apache License, Version 2.0",
+    author="Alexander Tikhonov",
+    author_email="random.gauss@gmail.com",
+    url="https://github.com/Fatal1ty/aioapns",
+    packages=find_packages(exclude=("tests",)),
+    package_data={"aioapns": ["py.typed"]},
+    install_requires=[
+        "h2>=4.0.0",
+        "pyOpenSSL>=17.5.0",
+        "pyjwt>=2.0.0",
+    ],
 )
